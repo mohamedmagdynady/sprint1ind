3e48d374ef0cb8e8240783a7c98b28a1aca00c1a
yasmeen hany <yhaah_97@hotmail.com>
2018-02-27 21:01:50 +0200

    added button, table and import to be able to use icons

--- src/app/dashboard/dashboard-menu.ts
+++ src/app/dashboard/dashboard-menu.ts
@@ -3,7 +3,7 @@ import { NbMenuItem } from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   { title: 'Dashboard', icon: 'nb-home', link: '/dashboard', home: true },
   {
-    title: 'Items',
+    title: 'Cart',
     icon: 'fa fa-shopping-basket',
     link: '/dashboard/items'
   },
